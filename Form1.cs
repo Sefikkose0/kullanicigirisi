6d6c498305a3fe8a060160cb5f95d0148283d30d
Şefik Köse <92820796+Sefikkose0@users.noreply.github.com>
2022-11-02 23:26:01 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ybsödev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "123456" && textBox1.Text == "admin123")
+            {
+                Form2 git = new Form2();
+                git.Show();
+                this.Hide();
+                MessageBox.Show("Başarılı Giriş yaptınız");
+            }
+
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Parola Hatalı","HATA");
+                textBox2.Clear();
+                textBox1.Clear();
+            }
+        }
+    }
+}
